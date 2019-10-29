7cc652bf1f4f75c31b50f9b211760b0932be852e
Kali Pulkkinen <kapulkkinen@gmail.com>
2019-10-29 13:52:52 -0400

    adding inital files

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,14 @@
+    // create an array of words 
+    var words = [""];
+    
+    
+    var win = 0;
+    var loss = 0;
+    var guessesLeft = 10;
+    var alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    var word = []
+    document.onkeyup = function(event) { 
+        var userGuess = event.key;
+
+    };
+
