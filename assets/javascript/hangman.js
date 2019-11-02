8c42ae2c3c31881a98caf6c1446f9bbf33aac176
Kali Pulkkinen <kapulkkinen@gmail.com>
2019-11-02 15:30:49 -0400

    button improvements and fixed user guess issue

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,20 +1,20 @@
-    // makes sure document is ready before running script
-    $(document).ready(function() {
+// makes sure document is ready before running script
+$(document).ready(function () {
 
     // create an array of words 
-    var words = ["mango", "banana", "cheese", "pistachio", "shrimp", 
-                "lemon", "broccoli", "squash", "avocado", "popsicle", 
-                "kiwi", "pancake", "cauliflower", "strawberry", "coconut", 
-                "cucumber", "carrot", "oatmeal", "almond", "kale"];
-    
+    var words = ["mango", "banana", "cheese", "pistachio", "shrimp",
+        "lemon", "broccoli", "squash", "avocado", "popsicle",
+        "kiwi", "pancake", "cauliflower", "strawberry", "coconut",
+        "cucumber", "carrot", "oatmeal", "almond", "kale"];
+
     //variables for wins, losses, guesses remanining and user options
     var win = 0;
     var loss = 0;
-    
+
     //creates variables that reference html in order to display things
     var userChoiceText = $("#userchoice-text");
     var computerChoiceText = $("#computerchoice-text");
-    var winsText = $("#wins-text");                                                                                                                                                                                                                                                
+    var winsText = $("#wins-text");
     var lossesText = $("#losses-text");
     var remainingText = $("#remaining-text");
 
@@ -32,29 +32,27 @@
         computerChoiceText.append("<span class=" + chosenWord[i] + ">" + answerArray[i] + "</span>");
     }
 
+    $("#remaining-text").text("guesses left: " + guessesLeft);
+
     // begins function to recognize users letter choice
-    document.onkeyup = function(event) { 
+    document.onkeyup = function (event) {
         var userGuess = event.key;
         $("#userchoice-text").append(" " + userGuess);
-        $("#remining-text").append(guessesLeft);
+        $("#remaining-text").text("guesses left: " + guessesLeft);
+        guessesLeft -= 1;
         console.log(guessesLeft);
 
-    //if userGuess is within the word array, split character & reaveal within word
-        splitChosenWord.forEach(function(letter) {
+        //if userGuess is within the word array, split character & reaveal within word
+        splitChosenWord.forEach(function (letter) {
             if (userGuess === letter) {
                 $('.' + letter).html(letter);
             }
-              
-            // define else statement to subtract from number of guesses remaining if letter guessed is not within array
-            //  if (userGuess !== letter) {
-            //     guessesLeft = --1;
-            // }    
-      
+
         });
 
-    // if/else statement to track wins & losses
-    // if word is guessed before guessesLeft = 0 add a win
-    // else word is not guessed before guessesLeft = 0 add loss
+        // if/else statement to track wins & losses
+        // if word is guessed before guessesLeft = 0 add a win
+        // else word is not guessed before guessesLeft = 0 add loss
 
         //  if (complete === true && guessesLeft > 0) {
         //     wins++;
@@ -64,14 +62,18 @@
         // } 
 
 
+
+
+    }
+
     //on button click, generate new word, but keep win and loss data intact 
-    $(".reset").on("click", function() {
-        $("#computerchoice-text").empty();
+    $("#reset").on("click", function () {
+        $(answerArray).empty();
         $("reamining-text").empty();
-         $("userchoice-text").empty();
-        })
-
-
-    }    
+        $("userchoice-text").empty();
+        var chosenWord = words[Math.floor(Math.random() * words.length)];
+        guessesLeft = chosenWord.length + 5;
+        console.log(chosenWord);
+    })
 
 });
